5f9097c03b89818f08094d35533792047ac7e5f1
Ricardo Signes <rjbs@cpan.org>
2012-10-01 14:19:40 -0400

    docs

--- lib/Email/MIME/Kit/Renderer/Text/Template.pm
+++ lib/Email/MIME/Kit/Renderer/Text/Template.pm
@@ -3,7 +3,7 @@ use Moose;
 with 'Email::MIME::Kit::Role::Renderer';
 # ABSTRACT: render parts of your mail with Text::Template
 
-use Text::Template ();
+use Module::Runtime ();
 
 sub _enref_as_needed {
   my ($self, $hash) = @_;
@@ -15,6 +15,26 @@ sub _enref_as_needed {
 
   return \%return;
 }
+
+=attr template_class
+
+This attribute stores the name of the class that will be standing in for
+Text::Template, if any.  It defaults, obviously, to Text::Template.
+
+=cut
+
+has template_class => (
+  is  => 'ro',
+  isa => 'Str',
+  default => 'Text::Template',
+);
+
+=attr template_args
+
+These are the arguments that will be passed to C<fill_this_in> along with the
+template, input, and a few required handlers.
+
+=cut
 
 has template_args => (
   is  => 'ro',
@@ -28,13 +48,17 @@ sub render  {
     (map {; $_ => ref $args->{$_} ? $args->{$_} : \$args->{$_} } keys %$args),
   });
 
-  my $result = Text::Template->fill_this_in(
+  my $template_class = $self->template_class;
+  Module::Runtime::require_module($template_class);
+
+  my $result = $template_class->fill_this_in(
     $$input_ref,
     %{ $self->{template_args} || {} },
     HASH   => $hash,
     BROKEN => sub { my %hash = @_; die $hash{error}; },
   );
 
+  # :-(  -- rjbs, 2012-10-01
   die $Text::Template::ERROR unless defined $result;
 
   return \$result;
